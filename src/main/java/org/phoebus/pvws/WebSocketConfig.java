c4c2795a1bbd436f538330f3596637d3b1f6bdaa
conocancer <conocancer@gmail.com>
2025-06-24 11:46:30 -0400

    stomp attempt 2

--- src/main/java/org/phoebus/pvws/WebSocketConfig.java
+++ src/main/java/org/phoebus/pvws/WebSocketConfig.java
@@ -17,14 +17,14 @@
  */
 package org.phoebus.pvws;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.phoebus.pvws.ws.PvSocketHandler;
-import org.phoebus.pvws.ws.WebSocket;
-import org.springframework.beans.factory.annotation.Autowired;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.phoebus.pvws.ws.PvSocketHandler;
+//import org.phoebus.pvws.ws.WebSocket;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+//import org.springframework.web.socket.config.annotation.EnableWebSocket;
+//import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+//import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry; 
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -45,7 +45,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/pvws").setAllowedOrigins("*").withSockJS();
+     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/pvws") // this matches what SockJS connects to
+                .setAllowedOriginPatterns("*")
+                .withSockJS(); // critical to support fallback transports
     }
 }
